01f987b8cf7c39477cddb25da4d085bc0910b0a7
daiki <link5105@gmail.com>
2020-03-06 14:34:07 +0900

    トップページ、ユーザー情報画面の修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,24 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new,:edit,:create,:update]
   def new
     @post = current_user.posts.build #if user_signed_in?
   end
 
   def edit
+    
   end
+
 
   def index
   end
 
   def create
     @post=current_user.posts.build(post_params)
-
-    # @post.save
     if @post.save
       redirect_to root_url, notice:"投稿しました。"
     else
-    
+      flash={danger: "〜〜失敗"}
+      render'new'
     end
   end
 
