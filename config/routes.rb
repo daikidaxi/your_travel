0d101ef3df461f31f5866266530360ca74a6312e
daiki <link5105@gmail.com>
2020-03-07 20:41:35 +0900

    簡易いいね機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
+
   get 'posts/new'
   get 'posts/edit'
   get 'posts/index'
   get 'users/index'
   get 'users/show'
+  
+  root 'top_page#home'
+
   devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
   devise_scope :user do
     get 'login', to: 'devise/sessions#new'
     post 'login', to: 'devise/sessions#create'
     delete 'signout', to: 'devise/sessions#destroy'
   end
-  
-  root 'top_page#home'
   resources :users
-  resources :posts
+  resources :posts do
+    resource :likes, only: [:create, :destroy]
+  end
+  
 end
