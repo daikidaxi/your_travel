ce9282c135a3f49b64bdccb8421c5b703a6e7c19
daiki <link5105@gmail.com>
2020-03-29 07:08:25 +0900

    add3 for heroku

--- db/migrate/20200229112131_add_column_to_users.rb
+++ db/migrate/20200229112131_add_column_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :name, :string, after: :id
+    add_column :users, :name, :sting, after: :id
     add_column :users, :nationality, :string
   end
 end
